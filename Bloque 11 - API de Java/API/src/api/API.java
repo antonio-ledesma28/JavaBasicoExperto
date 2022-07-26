ac48d214061b807d26f46dd02ac1b08970d73ab0
Antonio Yamir Ledesma Briones <54130633+antonio-ledesma28@users.noreply.github.com>
2022-07-25 22:54:24 -0500

    API

---
+++ Bloque 11 - API de Java/API/src/api/API.java
@@ -0,0 +1,13 @@
+
+package api;
+
+import java.util.Scanner;
+import java.util.Date;
+public class API {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        Date fecha;
+    }
+    
+}
